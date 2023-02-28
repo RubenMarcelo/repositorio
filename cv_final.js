f27c23c97d483a013c4d200f39dca1d9294a7150
Ruben Oviedo <rubenoviedo33@gmail.com>
2023-02-28 15:52:02 -0300

    Mi primer commit Cv Final

---
+++ cv_final.js
@@ -0,0 +1,6 @@
+
+
+document.getElementById('si_video').addEventListener('click', function() {
+        console.log("Hola mundo desde EventListenner");
+        document.getElementById("demo").innerHTML = "Estamos probando nuestro primer evento en js";
+    });
