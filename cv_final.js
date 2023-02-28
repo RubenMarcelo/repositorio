60ec19116f3ca2037cac78899463ff9303a8e02b
RubenMarcelo <rubenoviedo33@gmail.com>
2023-02-28 16:55:45 -0300

    Correcciones de tipos de letras. Correcciones de la función botón

--- cv_final.js
+++ cv_final.js
@@ -1,6 +1,6 @@
 
 
-document.getElementById('si_video').addEventListener('click', function() {
-        console.log("Hola mundo desde EventListenner");
-        document.getElementById("demo").innerHTML = "Estamos probando nuestro primer evento en js";
-    });
+document.getElementById('boton').addEventListener('click', function() {
+    document.getElementById("demo").innerHTML = 
+    "Disponibilidad Full time. Posibilidad de traslados dentro de el territorio de la provincia de Santa Fe. Poseo movilidad propia como así también equipamiento y herramientas propias para diseño.";
+});
